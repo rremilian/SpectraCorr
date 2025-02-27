35f0cd16d83215f5176e63a3521646fa215e6bba
Emilian R <92378685+rremilian@users.noreply.github.com>
2025-02-27 19:43:22 +0200

    Some fixes for OrcaParser

--- sc_io/parsers/OrcaParser.py
+++ sc_io/parsers/OrcaParser.py
@@ -7,21 +7,29 @@ class OrcaParser:
         with open(hess_file, 'r') as f:
             lines = f.readlines()
             lines = [line.strip() for line in lines]
-
         start_marker = f'${stype}_spectrum'
-        parsing = False
         data = []
-        for line in lines:
+        parsing = False
+        index = 0
+        
+        if stype == 'ir':
+            intensities_row = 2
+        elif stype == 'raman':
+            intensities_row = 1
+
+        while index < len(lines):
+            line = lines[index]
             if line.startswith('$') and parsing:
                 break
             elif line == start_marker:
+                index += 1 # Skip the number of atoms
                 parsing = True
-                continue
             elif parsing:
                 if line:
                     data.append(line)
+            index += 1
 
         data = np.array([line.split() for line in data], dtype=float)
         frequencies = data[:, 0]
-        intensities = data[:, 2]
+        intensities = data[:, intensities_row]
         return (frequencies, intensities)
