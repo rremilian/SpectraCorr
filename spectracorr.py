e34aa7987b9934d0dcc2e57995672e14e3f2ec32
Emilian R <92378685+rremilian@users.noreply.github.com>
2025-02-28 14:42:28 +0200

    Use global imports

--- spectracorr.py
+++ spectracorr.py
@@ -1,13 +1,14 @@
+from sc_io.parsers.CsvParser import CsvParser
+from sc_io.parsers.OrcaParser import OrcaParser
+
 class SpectraCorr:
     def __init__(self):
         pass
     def parseOrca(self, hess_file, stype):
-        from sc_io.parsers.OrcaParser import OrcaParser
         parser = OrcaParser()
         spectral_data = parser.parse_spectral_data(hess_file, stype)
         return spectral_data
     def parseCsv(self, csv_file, stype, freq_column = 0, int_column = 0):
-        from sc_io.parsers.CsvParser import CsvParser
         parser = CsvParser()
         spectral_data = parser.parse_spectral_data(csv_file, stype, 
                                                    freq_column=freq_column, 
