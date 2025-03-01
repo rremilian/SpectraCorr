2ddaab7032a737d3367c9bbee2993ac03f723a83
Emilian Rosca <emilian@local>
2025-03-01 19:37:34 +0200

    Remove freqlist and intlist from BaseGenerator class

--- generators/LorentzianGenerator.py
+++ generators/LorentzianGenerator.py
@@ -2,8 +2,8 @@ import numpy as np
 from generators.BaseGenerator import BaseGenerator
 
 class LorentzianGenerator(BaseGenerator):
-    def __init__(self, freqlist, intlist, fmin, fmax, step, sigma):
-        super().__init__(freqlist, intlist, fmin, fmax, step, sigma)
+    def __init__(self, fmin, fmax, step, sigma):
+        super().__init__(fmin, fmax, step, sigma)
 
     def distribution_function(self, freq, sigma, x):
         return (1.0/np.pi) * (sigma/((x-freq)**2 + sigma**2))
